b5eed7fd870dd0c97a232abc19ada98d59568dd1
Saigot <michael@michael-Vostro-1220>
2013-11-21 13:28:59 -0500

    moved TODO list to it's own file and added  to it.

--- src/pewpew/PewPew.java
+++ src/pewpew/PewPew.java
@@ -19,13 +19,6 @@ public class PewPew {
      *            the command line arguments
      */
     public static void main(String[] args) throws SlickException {
-        /*
-         * TODO:add more powerups (negative and positive)add more gunsadd more
-         * enemiesadd start screen (urggggg)finish pause menu create generic
-         * on-screen message system level system with bonuses, unlocks etc
-         * bosses: Giant Asteroid HighScores.txt, Options.txt options menu
-         * resizers don't stack perfectly
-         */
         StartScreen s = new StartScreen();
         s.build();
     }
